bf1111fb4d871b5c2ea77079f6a4c0e3c9f984ec
Jere Nurminen <jere.nurminen@reaktor.com>
2025-01-29 21:48:33 +0200

    Flesh out dice type

--- src/core/types.rs
+++ src/core/types.rs
@@ -1,3 +1,5 @@
+use macroquad::rand;
+
 pub type TurnNumber = u64;
 
 pub enum Dice {
@@ -11,8 +13,38 @@ pub enum Dice {
     Flat(u64),
 }
 
+impl Dice {
+    pub fn sides(&self) -> u64 {
+        match self {
+            Dice::D4 => 4,
+            Dice::D6 => 6,
+            Dice::D8 => 8,
+            Dice::D10 => 10,
+            Dice::D12 => 12,
+            Dice::D20 => 20,
+            Dice::D100 => 100,
+            Dice::Flat(sides) => *sides,
+        }
+    }
+}
+
 pub struct DieRoll {
     count: u64,
     dice: Dice,
     modifier: i64,
+}
+
+impl DieRoll {
+    fn roll(&self) -> i64 {
+        let roll_result = match self.dice {
+            Dice::Flat(sides) => sides * self.count,
+            _ => {
+                // Repeat this process count times
+                (0..self.count)
+                    .map(|_| rand::gen_range(1, self.dice.sides()))
+                    .sum()
+            }
+        };
+        roll_result as i64 + self.modifier
+    }
 }
